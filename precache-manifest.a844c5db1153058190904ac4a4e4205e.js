b611b1f093e9dca707f05dffe4fcd361759010ed
viss23 <wissper777@gmail.com>
2020-11-02 20:38:35 +0200

    Updates

---
+++ precache-manifest.a844c5db1153058190904ac4a4e4205e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd15a91102b41cfa3d0d911e2029e1f0",
+    "url": "/reenbit-chat/index.html"
+  },
+  {
+    "revision": "1317f4bc1f3eff3f9d03",
+    "url": "/reenbit-chat/static/css/main.a6785663.chunk.css"
+  },
+  {
+    "revision": "5b7d52c4a3572c5604fb",
+    "url": "/reenbit-chat/static/js/2.377a397c.chunk.js"
+  },
+  {
+    "revision": "de2613ebce8a261be82fa7c04c15983a",
+    "url": "/reenbit-chat/static/js/2.377a397c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1317f4bc1f3eff3f9d03",
+    "url": "/reenbit-chat/static/js/main.c04af353.chunk.js"
+  },
+  {
+    "revision": "8ffbb37bf7bcda0fbb7e",
+    "url": "/reenbit-chat/static/js/runtime-main.f633c381.js"
+  },
+  {
+    "revision": "445f2174d9b978e9cb0e91bf192bb937",
+    "url": "/reenbit-chat/static/media/search.445f2174.png"
+  },
+  {
+    "revision": "f3f2d5f5168f371f35cda171a8878ad7",
+    "url": "/reenbit-chat/static/media/send-message-6-663814.f3f2d5f5.png"
+  }
+]);
